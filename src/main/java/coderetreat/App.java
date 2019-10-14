c5a80bd53f85b2aac23ab7d84b70bf17305d043d
Ilie Ciotir <ilie.ciotir@sap.com>
2019-10-14 21:41:09 +0300

    make a starter

---
+++ src/main/java/coderetreat/App.java
@@ -0,0 +1,13 @@
+package coderetreat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
